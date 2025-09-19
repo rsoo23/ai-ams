7458a6614b50b10b9c9e27e0dda7faf6f1b73032
Wong Hong You <wonghongyou@gmail.com>
2025-09-19 17:46:58 +0800

    feat: Add CORS thingy to allow frontend to talk to backend without headache (hopefully)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 from app.api.v0.router import router as api_router
 
 app = FastAPI(title="ai-ams-backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/v0")
 
