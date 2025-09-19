a5a015fc75f8382ce7f67fd80b3813292d52ac0e
Wong Hong You <wonghongyou@gmail.com>
2025-09-19 17:47:30 +0800

    chore: Change S3 bucket name to the actual one in our AWS account

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,5 +5,4 @@ RDS_DB_NAME = os.getenv('RDS_DB_NAME', '')
 RDS_USERNAME = os.getenv('RDS_USERNAME', '')
 RDS_PASSWORD = os.getenv('RDS_PASSWORD', '')
 AWS_REGION = 'as-southeast-1'
-S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME', '')
-S3_KEY = os.getenv('S3_KEY', '')
+S3_BUCKET_NAME = 'ai-ams-documents'
