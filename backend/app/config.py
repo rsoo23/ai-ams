ef35a3913acb89b5b2182b2d488bf0d318334db9
Wong Hong You <wonghongyou@gmail.com>
2025-09-19 18:14:20 +0800

    chore: Fix incorrect AWS_REGION name

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,5 +4,5 @@ RDS_ENDPOINT = os.getenv('RDS_ENDPOINT', '')
 RDS_DB_NAME = os.getenv('RDS_DB_NAME', '')
 RDS_USERNAME = os.getenv('RDS_USERNAME', '')
 RDS_PASSWORD = os.getenv('RDS_PASSWORD', '')
-AWS_REGION = 'as-southeast-1'
+AWS_REGION = 'ap-southeast-1'
 S3_BUCKET_NAME = 'ai-ams-documents'
