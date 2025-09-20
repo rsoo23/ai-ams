50357eaefdb2b19082ed80b4d6233942010d242a
TESTERS <testers@Lewiss-MacBook-Air.local>
2025-09-20 15:47:04 +0800

    chore: Update config values

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,5 +4,5 @@ RDS_ENDPOINT = os.getenv('RDS_ENDPOINT', '')
 RDS_DB_NAME = os.getenv('RDS_DB_NAME', '')
 RDS_USERNAME = os.getenv('RDS_USERNAME', '')
 RDS_PASSWORD = os.getenv('RDS_PASSWORD', '')
-AWS_REGION = 'ap-southeast-1'
-S3_BUCKET_NAME = 'ai-ams-documents'
+AWS_REGION = 'ap-southeast-5'
+S3_BUCKET_NAME = 'ai-ams-bucket'
